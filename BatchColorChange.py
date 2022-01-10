ac2a7eda328cfef1df7ec2794670bf8e90cc60cf
Becca Pfeiffer <r.pfeiffer@utah.edu>
2022-01-10 12:59:55 -0700

    Added batch color change script and updated the rename script.

---
+++ BatchColorChange.py
@@ -0,0 +1,14 @@
+import bpy
+
+#create a material you'll be coloring
+mat = bpy.data.materials.new(name= 'colored')
+
+#iterate over objects in scene
+for o in bpy.context.scene.objects:
+    if "CBa" in o.name:
+        #Set the created material as the active material
+        o.active_material = mat
+        #Set colors using R,G,B formatting
+        mat.diffuse_color = (.004, .130, 1)
+
+
