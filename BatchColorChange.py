023be4bcb3274613f63b878313d79e8ccb531fc0
Becca Pfeiffer <r.pfeiffer@utah.edu>
2022-01-18 09:45:27 -0700

    Colors in Blender 3.0 have 4 (adding opacity) vs 2.79 which only has 3 (RGB)

--- BatchColorChange.py
+++ BatchColorChange.py
@@ -8,7 +8,7 @@ for o in bpy.context.scene.objects:
     if "CBa" in o.name:
         #Set the created material as the active material
         o.active_material = mat
-        #Set colors using R,G,B formatting
-        mat.diffuse_color = (.004, .130, 1)
+        #Set colors using R,G,B,opacity formatting
+        mat.diffuse_color = (.004, .130, 1, 1)
 
 
