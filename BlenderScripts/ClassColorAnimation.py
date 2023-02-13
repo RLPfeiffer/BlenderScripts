e92ce2f2c805a2e43afbfd6f176d52ff4c269dab
RLPfeiffer <r.pfeiffer@utah.edu>
2023-02-13 15:18:38 -0700

    Added structure colors

--- BlenderScripts/ClassColorAnimation.py
+++ BlenderScripts/ClassColorAnimation.py
@@ -1,8 +1,8 @@
 import bpy
 
-#Save all of your cell_class colors to the index and access them there to make changing colors easy
-cell_class = 'PR(cone)'
-alphaChange = bpy.data.collections[cell_class]
+#Save all of your structure colors to the index and access them there to make changing colors easy
+structure = 'PR(cone)'
+alphaChange = bpy.data.collections[structure]
 colorIndex = [
 (0.03, 0, 0.6, 1), #PR(cone) = [0]
 (0.6, 0.5, 0, 1), #PR(ind) = [1]
@@ -13,8 +13,13 @@ colorIndex = [
 (0.02, 0, 1, 1), #RodBC = [6]
 (0.2, 0, 0, 1), #GC = [7]
 (0.6, 0.7, 0, 1), #HC = [8]
-(1, 0.5, 0, 1), #MC = [9]
-(1, 0, 0, 1) #yAC = [10]
+(0.319, .284, 0.046, 1), #MC = [9]
+(1, 0, 0, 1), #yAC = [10]
+(0.907, 0, 0.929, 1), #AJ = [11]
+(0, 0.929, .308, 1), #Cav = [12]
+(1, 0, 0, 1), #GAdj_Inhib = [13]
+(0, 1, 0, 1), #GAdj_Ribbon = [14]
+(0.104, 0, 1, 1), #NGAJ = [15]
 ]
 
 for ob in alphaChange.objects:
